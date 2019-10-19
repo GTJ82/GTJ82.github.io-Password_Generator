c717e9a4a2a4a6273f7ae98f0c4a52d22f771899
Josh Eskew <jeskew82@gmail.com>
2019-10-19 18:49:00 -0400

    started script.js. Only named variables and started functions

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
+
+var password = ""
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
+
+document.getElementById("display_box"). values = password
+
