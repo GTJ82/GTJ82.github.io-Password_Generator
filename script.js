eb203a9c77d82729dfd406b7e0dace42c780b189
Josh Eskew <jeskew82@gmail.com>
2019-10-20 17:42:40 -0400

    Added more to script.js but still incomplete

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
-var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
-
+function genPassword()
+var values = ["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"]
 var password = ""
 
 for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+   var c = Math.floor(Math.random() * values.length + 1)
+   password += chars.charAt(c)
     
 }
 
-document.getElementById("display_box"). values = password
-
+  console.log(password)
