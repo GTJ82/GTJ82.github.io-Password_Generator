6e6e640f2abb85bd55e82efd3067d34faaa26c21
Josh Eskew <jeskew82@gmail.com>
2019-10-20 21:09:09 -0400

    added one more addition to the user info inout section

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var getPassword = confirm("do you want to generate a password?")
+var getPassword = confirm("Do you want to generate a password?")
     if( getPassword === true)
         alert("Password can include all letters, numbers and special characters")
 
-var charNumber = prompt("How many characters do you want the password to be? needs to have a minimum of and and a max of 128.")
-    if( charNumber >= 8 && charNumber >= 128)
-        genPassword()
-
+var charNumber = prompt("How many characters do you want the password to be? Needs to have a minimum of 8 and and a max of 128.")
+    if (charNumber < 8 && charNumber > 128)
+        alert ( "Character length must be between 8 and 128!")
+            else if( charNumber >= 8 && charNumber >= 128)
+                genPassword()
 
 
 
@@ -15,7 +16,7 @@ var password = "";
 var array = [];
 
 
-for (let i = 0; i < values.length; i++) {
+for (let i = 0; i < charNumber; i++) {
    var c = Math.floor(Math.random() * values.length + 1)
    password += values.charAt(c)
     
