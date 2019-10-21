bee02d8ad36abc8ce829ed9fb24ebdd5bf4259c3
Josh Eskew <jeskew82@gmail.com>
2019-10-20 20:03:43 -0400

    added some javascript. Added a confirm and prompt asking for user input

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var getPassword = confirm("do you want to generate a password?")
+    if( getPassword === true)
+        alert("Password can include all letters, numbers and special characters")
+
+var charNumber = prompt("How many characters do you want the password to be? needs to have a minimum of and and a max of 128.")
+    if( charNumber >= 8 && charNumber >= 128)
+        genPassword()
+
+
+
+
 function genPassword(){
 var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 var password = "";
