1dd78c3b68f87507be0ff0581f0bf3a963ea0603
Josh Eskew <jeskew82@gmail.com>
2019-10-29 21:11:17 -0400

    Added the rest of my js

--- script.js
+++ script.js
@@ -1,26 +1,71 @@
-var getPassword = confirm("Do you want to generate a password?")
-    if( getPassword === true)
-        alert("Password can include all letters, numbers and special characters")
+var specChar = [!@#$ %^&* ()_ +];
+var uppercase = [ABCDEFGHIJKLMNOPQRSTUVWXYZ];
+var lowercase = [abcdefghijklmnopqrstuvwxyz];
+var nums = [1234567890];
 
-var charNumber = prompt("How many characters do you want the password to be? Needs to have a minimum of 8 and and a max of 128.")
-    if (charNumber < 8 && charNumber > 128)
-        alert ( "Character length must be between 8 and 128!")
-            else if( charNumber >= 8 && charNumber >= 128)
-                genPassword()
+var passwordArray = [];
+var count;
+
+function genPassword() {
+
+    document.getElementById(generate).addEventListener("click", function () {
+        length();
+    })
+
+    function length() {
+        count = prompt("How long of a password would you like? Must bewtween 8 and 128 characters.")
+        if (count > 128) {
+            alert("Must be less than 128 characters!")
+            length()
+        }
+        else if (count < 8 ){
+            alert("Must be at least 8 characters long!")
+            length()
+        }
+        else {
+            character()
+        }
+    }
 
 
+    for (let i = 0; i < charNumber; i++) {
+        var c = Math.floor(Math.random() * values.length + 1)
+        password += values.charAt(c)
 
-function genPassword(){
-var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
-var password = "";
-var array = [];
+    }
 
-
-for (let i = 0; i < charNumber; i++) {
-   var c = Math.floor(Math.random() * values.length + 1)
-   password += values.charAt(c)
-    
 }
-console.log(password)
+
+function character(){
+    passwordArray=[];
+    if(confirm("Would you like special characters in your password?")){
+        passwordArray.push(specChar);
+    }
+    if(confirm("Would you like numbers in your password?")){
+        passwordArray.push(nums);
+    }
+    if(confirm("Would you like uppercase letters in your password?")){
+        passwordArray.push(uppercase);
+    }
+    if(confirm("Would you like lowercase letters in your password?")){
+        passwordArray.push(lowercase);
+    }
+    if(passwordArray.length < 2){
+        alert("Must select at least 2 conditions!");
+        character();
+    }
+    else{
+        generate(passwordArray,count)
+    }
+
+}
+
+function generate(passwordArray,passwordlength){
+var password = i;
+var passwordlength = parseInt(passwordlength);
+    for(var k = 0; k < passwordlength; k++){
+        var whichArray = Math.floor(Math.random() * passwordArray.length)
+        password += passwordArray[whichArray[Math.floor(Math.random() * passwordArray[whichArray].length)]]
+    }
 }
 
---
+++ script2.js
@@ -0,0 +1,12 @@
+var specialChar = ["!@#$%^&*()_+"];
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVQXYZ"];
+var numbers = ["0123456789"];
+
+var passwordArray = [];
+
+var count;
+
+document.getElementById("first").addEventListener("click", function()
+    alert("yo")
+)}
